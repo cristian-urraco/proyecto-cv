207d43064182b546f73994a04d305d06552cbe93
cristian-urraco <123096227+cristian-urraco@users.noreply.github.com>
2023-01-19 13:28:23 -0300

    primer subida de archivos a repositorio
    subo los tres archivos que creo necesarios html.css.js

---
+++ cv.js
@@ -0,0 +1,20 @@
+
+  document.getElementById("logros1").addEventListener('click', function() {
+    if (document.getElementById("lista_de_logros1").style.display=='block'){
+      document.getElementById("lista_de_logros1").style.display='none'
+    }
+    else{ 
+      document.getElementById("lista_de_logros1").style.display='block'
+    }  
+ })  
+
+ document.getElementById("logros2").addEventListener('click', function() {
+  if (document.getElementById("lista_de_logros2").style.display=='block'){
+    document.getElementById("lista_de_logros2").style.display='none'
+  }
+  else{ 
+    document.getElementById("lista_de_logros2").style.display='block'
+  }  
+})  
+  
+    
